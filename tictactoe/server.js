0e21e1b8a45225252461a0f77478445be83abde5
Gabriel Fernandes <gabrielfernndss@gmail.com>
2017-09-20 16:14:27 -0300

    2/10

--- tictactoe/server.js
+++ tictactoe/server.js
@@ -44,29 +44,60 @@ function getIndex(token) {
     }
 }
 setInterval(() => {
+    console.log(connections)
     if (connections.length > maxConnections/2)
         process.exit(15)
     for (let i = 0; i < connections.length; i++) {
-        connections[i][0]--
-        connections[i][1]--
-        if (connections[i][0] < 1)
-            connections.splice(i, 1)
+        if (connections[i][3] == 'X') {
+            connections[i][0]--
+            if (connections[i][0] < 1)
+                connections.splice(i, 1)
+        } else {
+            connections[i][1]--
+            if (connections[i][1] < 1)
+                connections.splice(i, 1)
+        }
     }
 }, 1000)
 
 http.createServer((req, res) => {
     let url = parse(req.url),
         path = url.pathname,
-        query = url.query
+        query = url.query,
+        info = querystring.parse(query)
+
     if (path == '/connect') {
         res.end(JSON.stringify(getConnection()))
+    } else if (path == '/init') {
+        let index = getIndex(info.token)
+        if (index != undefined) {
+            req.on('data', (data) => {
+                connections[index][0] = timeout
+                connections[index][3] = 'Y'
+                connections[index][4] = JSON.parse(data.toString())
+            })
+        }
+        res.end()
     } else if (path == '/turn') {
-        let info = querystring.parse(query),
-            index = getIndex(info.token)
-        req.on('data', (data) => {
-            //console.log(JSON.parse(data.toString()))
-            res.end(JSON.stringify(getConnection()))
-        })
+        let index = getIndex(info.token)
+        if (index != undefined && connections[index][3] == info.char) {
+            req.on('data', (data) => {
+                if (info.char == 'X') {
+                    connections[index][0] = timeout
+                    connections[index][3] = 'Y'
+                } else {
+                    connections[index][1] = timeout
+                    connections[index][3] = 'X'
+                }
+                let buffer = connections[index][4]
+                connections[index][4] = JSON.parse(data.toString())
+                res.end(JSON.stringify(buffer))
+            })
+        } else {
+            res.end()
+        }
+    } else if (path == '/timeout') {
+        res.end(`${timeout}`)
     } else if (path == '/assets/Tic_tac_toe.png') {
         res.end(logo)
     } else if (path == '/assets/dot-paper.png') {
